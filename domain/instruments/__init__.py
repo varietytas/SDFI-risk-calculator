59c57aa9935df986b22f51a3b490d47cfa610e75
varietytas <kuptsov.maksim2901@gmail.com>
2025-12-26 15:40:00 +0300

    add FxSwap domain model

--- domain/instruments/__init__.py
+++ domain/instruments/__init__.py
@@ -1,5 +1,6 @@
 from .contract import Contract
+from .curr_swap import FxSwap
 from .directions import Direction
 from .forward import FxFwd, FxNdf
 
-__all__ = ["Contract", "Direction", "FxFwd", "FxNdf"]
+__all__ = ["Contract", "Direction", "FxFwd", "FxNdf", "FxSwap"]
---
+++ domain/instruments/curr_swap.py
@@ -0,0 +1,41 @@
+from datetime import date
+
+from .directions import Direction
+from .contract import Contract
+
+
+class FxSwap(Contract):
+
+    def __init__(
+        self,
+        name: str,
+        registration_date: date,
+        maturity: int,
+        start_date: date,
+        end_date: date,
+        direction: Direction,
+        price: float,   # Swap points
+        rate: float,    # Exchange rate
+        currency_1: str,
+        amount_1: float,
+        currency_2: str,
+        amount_2: float,
+    ):
+        super().__init__(
+            product='FX Swap',
+            name=name,
+            registration_date=registration_date,
+            maturity=maturity,
+            start_date=start_date,
+            end_date=end_date,
+            direction=direction,
+            price=price,
+        )
+        self.rate = rate
+        self.currency_1 = currency_1
+        self.amount_1 = amount_1
+        self.currency_2 = currency_2
+        self.amount_2 = amount_2
+
+    def __repr__(self):
+        return f'{self.product} {self.maturity}D: {self.direction.value} {self.amount_1} {self.currency_1} for {self.amount_2} {self.currency_2} ({self.rate} exchange rate) on {self.start_date}, reverse on {self.end_date} with {self.price} swap points'
