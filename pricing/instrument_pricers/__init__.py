1d300fb3ccc5d3f765829fdbaa982b96fc0345ed
varietytas <kuptsov.maksim2901@gmail.com>
2026-03-27 11:40:00 +0300

    cleanup pricing subpackage

--- pricing/instrument_pricers/__init__.py
+++ pricing/instrument_pricers/__init__.py
@@ -1,5 +1,16 @@
-from .fx_pricer import FxForwardPricer
-from .fx_swap_pricer import FxSwapPricer
-from .irs_pricer import IRSwapPricer
+"""
+Instrument-specific pricers for calculating NPV and Liquidation Cost.
+"""
 
-__all__ = ['FxForwardPricer', 'FxSwapPricer', 'IRSwapPricer']
+from .fx_pricer import FxForwardPricer
+from .fx_fwd_lc_pricer import FxForwardLCPricer
+from .fx_swap_pricer import FxSwapPricer
+from .fx_swap_lc_pricer import FxSwapLCPricer
+from .irs_pricer import IRSwapPricer
+from .irs_lc_pricer import IRSwapLCPricer
+
+__all__ = [
+    'FxForwardPricer', 'FxForwardLCPricer',
+    'FxSwapPricer', 'FxSwapLCPricer',
+    'IRSwapPricer', 'IRSwapLCPricer',
+]
